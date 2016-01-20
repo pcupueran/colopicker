6d1078151182f3981c4b85a368a5a761d1fafef5
Patricia <pa1809ty@gmail.com>
2016-01-20 08:43:47 +0000

    Exercise using JQuery

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
 /* Exercise 2: Color picker */
+
+var color_list=[];
+
+$(document).ready(function() {
+
+  window.addEventListener("keydown", keydownHandler, false);
+
+  function keydownHandler(event) {
+    if(event.keyCode === 13) {
+      var color = $("input#color").val();
+      setPreview(color);
+    }
+  }
+
+  $(document).on('click', '#add-to-favorite', function() {
+    var color = $("input#color").val();
+    addBox(color);
+  });
+});
+
+function setPreview(color) {
+  $(".preview").css('background-color', color);
+}
+
+function addBox(color) {
+  var isEqual = false;
+
+  if (color_list.length === 0) {
+    var content = "<div class='item' style='background-color:" + color + ";'></div>";
+    $("#colors").prepend(content);
+    $("#color").val("");
+    setPreview("white");
+    color_list.push(color);
+  }
+  else if($(".item").length<16) {
+    for(var i = 0; i < color_list.length; i++) {
+      if(color_list[i] === color) {
+        isEqual = true;
+      }
+    }
+    if(!isEqual) {
+      var content = "<div class='item' style='background-color:" + color + ";'></div>";
+      $("#colors").prepend(content);
+      $("#color").val("");
+      setPreview("white");
+      color_list.push(color);
+    }
+    else {
+      console.log("The color has already been added to your favourite list");
+    }
+  }
+  else {
+    console.log("The maximum number of favourite colors is 16");
+  }
+}
